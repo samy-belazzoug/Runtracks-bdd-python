2cbd268ed8465948861a42bcbd7b88a833b140e8
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-19 14:37:42 +0100

    Exported database from Python (job03)

--- jour02/job03.py
+++ jour02/job03.py
@@ -1,11 +1,13 @@
 import mysql.connector
 from dotenv import load_dotenv
 import os
+import subprocess
 
 load_dotenv()
 db_host = os.getenv("DB_HOST")
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAMElp")
 
 mydb = mysql.connector.connect(
     host = db_host,
@@ -23,11 +25,11 @@ val = [
     ("R+1",1,500)
 ]
 
-mycursor.executemany(sql,val)
-mydb.commit()
+#mycursor.executemany(sql,val)
+#mydb.commit()
 
-print("Entries successfully added.")
-print(mycursor.rowcount,"was inserted.")
+#print("Entries successfully added.")
+#print(mycursor.rowcount,"was inserted.")
 
 mycursor.execute("SELECT * FROM etage")
 for x in mycursor:
@@ -43,12 +45,27 @@ val = [
     ("Studio Video",2,5)
 ]
 
-mycursor.executemany(sql,val)
-mydb.commit()
+#mycursor.executemany(sql,val)
+#mydb.commit()
 
-print("Entries successfully added.")
-print(mycursor.rowcount,"was inserted.")
+#print("Entries successfully added.")
+#print(mycursor.rowcount,"was inserted.")
 
 mycursor.execute("SELECT * FROM salle")
 for x in mycursor:
     print(x,"\n")
+
+"""EXPORTATION DE LA BASE DE DONNEES"""
+
+#nom du fichier de sauvegarde
+backup_file = f"{db_name}_backup.sql"
+
+#Construire la commande mysqldump
+cmd = f"mysqldump -h {db_host} -u {db_user} -p{db_password} {db_name} > {backup_file}"
+
+#Execution de la commande
+try:
+    subprocess.run(cmd,shell=True,check=True)
+    print(f"Sauvegarde reussie : {backup_file}")
+except subprocess.CalledProcessError as e:
+    print(f"Erreur lors de l'export : {e}")
