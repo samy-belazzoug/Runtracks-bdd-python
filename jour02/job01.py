416bfd7beba4c926cfda9903b25a9314c44548c7
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-19 13:49:58 +0100

    TEST COMMIT

---
+++ jour02/job01.py
@@ -0,0 +1,15 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+mydb = mysql.connector.connect(
+    host = db_host,
+    user = db_user,
+    password = db_password
+)
+print("Connected.")
