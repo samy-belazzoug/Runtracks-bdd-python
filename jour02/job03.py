3cbcf0018804d4b784e6a8716e4ad762dc884e3b
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-19 14:24:33 +0100

    Job 3 Done.

--- jour02/job03.py
+++ jour02/job03.py
@@ -15,3 +15,40 @@ mydb = mysql.connector.connect(
 print("Connected.")
 
 mycursor = mydb.cursor()
+mycursor.execute("USE laplateforme")
+
+sql = "INSERT INTO etage (nom,numero,superficie) VALUES (%s,%s,%s)"
+val = [
+    ("RDC",0,500),
+    ("R+1",1,500)
+]
+
+mycursor.executemany(sql,val)
+mydb.commit()
+
+print("Entries successfully added.")
+print(mycursor.rowcount,"was inserted.")
+
+mycursor.execute("SELECT * FROM etage")
+for x in mycursor:
+    print(x,"\n")
+
+sql = "INSERT INTO salle (nom,id_etage,capacite) VALUES (%s,%s,%s)"
+val = [
+    ("Lounge",1,100),
+    ("Studio son",1,5),
+    ("Broadcasting",2,50),
+    ("Bocal pPeda",2,4),
+    ("Coworking",2,80),
+    ("Studio Video",2,5)
+]
+
+mycursor.executemany(sql,val)
+mydb.commit()
+
+print("Entries successfully added.")
+print(mycursor.rowcount,"was inserted.")
+
+mycursor.execute("SELECT * FROM salle")
+for x in mycursor:
+    print(x,"\n")
